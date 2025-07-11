7c0003aa12ec7388c39f1bfdcee0f568e9981d0d
Bruno NItsche <nitsche0202@gmail.com>
2025-07-11 15:47:05 -0300

    Create ex3_revisao.c

---
+++ ap2_lista01_revisao/ex3_revisao.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int main() {
+
+	char estado_civil;
+
+	printf("Digite os estado civil");
+	scanf("%s", &estado_civil);
+
+	switch (estado_civil)
+	{
+	case 'c':
+		printf("IndicaC'C#o: Caramel Sweet & Salt");
+		break;
+
+	case 's':
+		printf("IndicaC'C#o: London Pub");
+		break;
+
+	case 'v':
+		printf("XI Baile da Melhor Idade");
+		break;
+
+	case 'd':
+		printf("XI Baile da Melhor Idade");
+		break;
+
+	}
+	return 0;
+}

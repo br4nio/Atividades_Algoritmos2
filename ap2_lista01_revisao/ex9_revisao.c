1cc91e93a42b7950953458327c3c8dc795a48c0c
Bruno NItsche <nitsche0202@gmail.com>
2025-07-11 20:15:29 -0300

    Update ex9_revisao.c

--- ap2_lista01_revisao/ex9_revisao.c
+++ ap2_lista01_revisao/ex9_revisao.c
@@ -1,1 +1,33 @@
+#include <stdio.h>
+
+int main() {
+    int matriz[4][5];
+    int i, j;
+    int menor, linhaMenor, colunaMenor;
+
+    printf("Digite os elementos da matriz 4x5:\n");
+    for(i = 0; i < 4; i++) {
+        for(j = 0; j < 5; j++) {
+            printf("Elemento [%d][%d]: ", i, j);
+            scanf("%d", &matriz[i][j]);
+
+            if(i == 0 && j == 0) {
+                menor = matriz[i][j];
+                linhaMenor = i;
+                colunaMenor = j;
+            } else {
+                if(matriz[i][j] < menor) {
+                    menor = matriz[i][j];
+                    linhaMenor = i;
+                    colunaMenor = j;
+                }
+            }
+        }
+    }
+
+    printf("\nMenor valor: %d\n", menor);
+    printf("Posição: linha %d, coluna %d\n", linhaMenor, colunaMenor);
+
+    return 0;
+}
 

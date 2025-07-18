fc152a3382cf5e85b1d3ebbbdfa3b8aec330e8e3
Bruno NItsche <nitsche0202@gmail.com>
2025-07-18 15:18:55 -0300

    Update ex1_funcoes.c

--- ap2_lista01_funcoes/ex1_funcoes.c
+++ ap2_lista01_funcoes/ex1_funcoes.c
@@ -1,1 +1,19 @@
+#include <stdio.h>
+
+int dobro(int numero) {
+    return numero * 2;
+}
+
+int main() {
+    int n, resultado;
+
+    printf("Digite um número: ");
+    scanf("%d", &n);
+
+    resultado = dobro(n);
+
+    printf("O dobro é %d", resultado);
+
+    return 0;
+}
 
